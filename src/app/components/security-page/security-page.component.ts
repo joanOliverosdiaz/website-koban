0e8ae014aa1d484dd42419d453a6381dd1beb5a4
joanOliverosdiaz <joan.oliveros@obsidiam.com>
2022-01-02 18:59:02 -0500

    new components

--- src/app/components/security-page/security-page.component.ts
+++ src/app/components/security-page/security-page.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./security-page.component.css']
 })
 export class SecurityPageComponent implements OnInit {
-  public isCollapsed = false;
+
   constructor() { }
 
   ngOnInit(): void {
